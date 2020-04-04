414b86f4f1602e2fe799f7e818b0b1232fd8e07d
zpj7377327 <58630290+zpj7377327@users.noreply.github.com>
2020-04-04 20:03:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/coolsnowwolf/lede/package
+git clone https://github.com/destan19/OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan
+./scripts/feeds update -a
+./scripts/feeds install -a
