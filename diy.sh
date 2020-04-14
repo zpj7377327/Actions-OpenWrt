80ef7546f45c718f5b603016926c690228e00fe8
zpj7377327 <58630290+zpj7377327@users.noreply.github.com>
2020-04-14 16:05:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,9 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git pull https://github.com/coolsnowwolf/lede
+git pull
 git clone https://github.com/coolsnowwolf/lede/package
+cd openwrt
 git clone https://github.com/destan19/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan
 ./scripts/feeds update -a
