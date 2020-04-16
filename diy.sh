217e5146e35de40cd6c14258b92d4e2f617f423c
zpj7377327 <58630290+zpj7377327@users.noreply.github.com>
2020-04-16 19:09:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git pull https：//github.com/coolsnowwolf/lede/packages
 git clone https://github.com/coolsnowwolf/lede/package
-cd openwrt
 git clone https://github.com/destan19/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan
 ./scripts/feeds update -a
